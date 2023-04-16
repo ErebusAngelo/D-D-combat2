05cf3452f0ecde87beb06db083c467ff1edc50ac
ErebusAngelo <99561421+ErebusAngelo@users.noreply.github.com>
2023-04-16 17:14:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-/// Definimos un array vacío para guardar los personajes
+// Definimos un array vacío para guardar los personajes
 let personajes = [];
 
 // Función que agrega un personaje a la lista
@@ -7,14 +7,17 @@ function agregarPersonaje() {
   // Obtenemos los valores de los inputs
   const nombre = document.getElementById("nombre").value;
   let combate = document.getElementById("combate").value;
+  let PGM= document.getElementById("PGM").value;
   let iniciativa = document.getElementById("iniciativa").value;
   const CA = document.getElementById("CA").value;
   // Creamos un objeto con los valores
   const personaje = {
     nombre: nombre,
     combate: combate,
+    PGM: PGM,
     iniciativa: iniciativa,
-    CA: CA
+    CA: CA,
+    
   };
 
   // Agregamos el objeto al array
@@ -23,12 +26,15 @@ function agregarPersonaje() {
   // Limpiamos los inputs
   document.getElementById("nombre").value = "";
   document.getElementById("combate").value = "";
+  document.getElementById("PGM").value = "";
   document.getElementById("iniciativa").value = "";
   document.getElementById("CA").value = "";
 
+
+
   // Ordenamos el array por iniciativa de mayor a menor
   personajes.sort((a, b) => b.iniciativa - a.iniciativa);
-
+  
   // Actualizamos la lista de personajes
   actualizarLista();
 }
@@ -37,26 +43,8 @@ function agregarPersonaje() {
 function actualizarLista() {
   const lista = document.getElementById("lista");
   lista.innerHTML = "";
-
-  // Recorremos el array de personajes y creamos un elemento li por cada uno
-  personajes.forEach((personaje, index) => {
-    const li = document.createElement("li");
-
-    li.innerHTML = `
-      <span>${personaje.nombre} - Iniciativa <input type="number" id="ini" min="0" value="${personaje.iniciativa}" onchange="modificarAtributo(${index}, 'iniciativa', event.target.value)"> - CA: ${personaje.CA} </span>
-      <span>
-        <button onclick="modificarPG(${index}, -1)">-</button>
-        Puntos de Golpe: ${personaje.combate}
-        <button onclick="modificarPG(${index}, 1)">+</button>
-      </span>
-      <label for="Estado"></label>
-      <input type="" placeholder ="Estado" id="Estado" name="Estado">
-      <label for="Estado"></label>
-      <input type="" placeholder ="PG Temporales" id="Temporal" name="Temporal">
-      <button onclick="eliminarPersonaje(${index})">Eliminar</button>
-    `;
-    lista.appendChild(li);
-  });
+  
+  
 }
 
 // Función que modifica los puntos de golpe de un personaje en el array y actualiza la lista
@@ -115,34 +103,70 @@ function eliminarPersonaje(index) {
   actualizarLista();
 }
 
+// Agregamos una variable global para almacenar una referencia al botón global
+let btnGlobal;
 // Función que actualiza la lista de personajes
 function actualizarLista() {
   const lista = document.getElementById("lista");
-  lista.innerHTML = "";
+
+  // Eliminamos todos los elementos li existentes
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
 
   // Recorremos el array de personajes y creamos un elemento li por cada uno
   personajes.forEach((personaje, index) => {
     const li = document.createElement("li");
-    
+    hp = personaje.combate 
     li.innerHTML = `
       <span>${personaje.nombre} - Iniciativa <input type="number" id="ini" min="0" value="${personaje.iniciativa}" onchange="modificarAtributo(${index}, 'iniciativa', event.target.value)"> - <img src="
       ./CA.png" alt="Descripción de la imagen"> ${personaje.CA} </span>
       <span>
         <button onclick="restarPuntosDeGolpe(${index})">-</button>
-        Puntos de Golpe: ${personaje.combate}
+        PG actuales: ${personaje.combate}
         <button onclick="sumarPuntosDeGolpe(${index})">+</button>
       </span>
+      <span>PG Maximos: ${personaje.PGM}</span>
       <label for="Estado"></label>
       <input type="number" placeholder ="PG temporales" id="PGT" class="bar" name="PGT">
       <label for="Temporal"></label>
       <input type="text" placeholder ="Estado" id="Estado" class="bar" name="Estado">
       <button onclick="eliminarPersonaje(${index})">Eliminar</button>
     `;
+    
+    // Agregamos el elemento li a la lista
     lista.appendChild(li);
   });
+
+  // Creamos el botón global si aún no existe
+  if (!btnGlobal) {
+    btnGlobal = document.createElement("button");
+    btnGlobal.textContent = "Descanso largo";
+    btnGlobal.onclick = actualizarTodosLosPG;
+    document.body.appendChild(btnGlobal);
+  }
+}
+
+
+
+
+// Agregamos la función actualizarTodosLosPG()
+function actualizarTodosLosPG() {
+  // Recorremos el array de personajes y actualizamos los valores de combate
+  personajes.forEach((personaje) => {
+    personaje.combate = personaje.PGM;
+  });
+  actualizarLista();
+}
+
+// Agregamos la función reemplazarPuntosDeGolpe()
+function reemplazarPuntosDeGolpe(index) {
+  personajes[index].combate = personajes[index].PGM;
+  actualizarLista();
 }
 const cambiarTurnoBtn = document.getElementById("cambiar-turno");
 let turnoActual = 0;
+
 
 function cambiarTurno() {
   const personajes = document.querySelectorAll("li");
@@ -152,4 +176,6 @@ function cambiarTurno() {
 }
 
 cambiarTurnoBtn.addEventListener("click", cambiarTurno);
+
+
 
